8d7aa4fdc4ac511d1590e67cdd20a3e569ef6f8b
galiwali <maringaliany@gmail.com>
2025-12-01 08:32:33 -0600

    Update 4 — Nested Lists and List Comprehensions.py

--- week 13  csp/4 — Nested Lists and List Comprehensions.py
+++ week 13  csp/4 — Nested Lists and List Comprehensions.py
@@ -30,6 +30,28 @@ print(matrix[1][2])    # 6
 first_col = [row[0] for row in matrix]
 print(first_col)       # [1, 4, 7]
 
+["apple", "orange, banana", "coconut"]
+["celery", "carrots", "potatoes"]
+["chicken", "fish", "turkey"]
+
+groceries = [["apple", "orange", "banana", "coconut"],
+["celery", "carrots", "potatoes"],
+["chicken", "fish", "turkey"]]
+for collection in groceries:
+    for food in collection:
+        print(food, end= " ")
+        print()
+
+num_pad = ((1, 2, 3),
+           (4, 5, 6),
+           (7, 8, 9),
+           ("*", 0, "#"))
+            
+for row in num_pad:
+    for num in row:
+        print(num, end=" ")
+    print()
+           
 
 
 # Practice Problems:
